7397b6d255e6355cafea76cab76d11f4c999b523
Segun Oroyo <shezvar@gmail.com>
2024-11-05 08:25:42 +0000

    test-3

--- next.config.ts
+++ next.config.ts
@@ -8,16 +8,25 @@
 
 
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+// const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   assetPrefix: isGithubActions ? '/shezvar.github.io/index' : '', // Update with your repo name
+//   basePath: isGithubActions ? '/shezvar.github.io/index' : '', // Update with your repo name
+//   trailingSlash: true, // Add this line
+//   distDir: 'docs', // Output directory for GitHub Pages
+//   // ... other configurations
+// };
+
+// module.exports = nextConfig;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/index",
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-  assetPrefix: isGithubActions ? '/shezvar.github.io/index' : '', // Update with your repo name
-  basePath: isGithubActions ? '/shezvar.github.io/index' : '', // Update with your repo name
-  trailingSlash: true, // Add this line
-  distDir: 'docs', // Output directory for GitHub Pages
-  // ... other configurations
 };
 
 module.exports = nextConfig;
