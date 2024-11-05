424fabc0cdcd833ef176ea842d14390528b1364d
Segun Oroyo <shezvar@gmail.com>
2024-11-05 08:04:30 +0000

    setup nextjs for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: isGithubActions ? '/shezvar.github.io/' : '', // Update with your repo name
+  basePath: isGithubActions ? '/shezvar.github.io/' : '', // Update with your repo name
+  trailingSlash: true, // Add this line
+  distDir: 'docs', // Output directory for GitHub Pages
+  // ... other configurations
 };
 
-export default nextConfig;
+module.exports = nextConfig;
